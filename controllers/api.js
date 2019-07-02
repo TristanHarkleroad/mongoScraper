8c027d25b5d9de7502f0c2da8fa335a1d662e40a
Tristan Harkleroad <tristan.harkleroad@gmail.com>
2019-07-01 21:16:41 -0400

    fixed a rerouting issue once scrape was completed.

--- controllers/api.js
+++ controllers/api.js
@@ -4,53 +4,62 @@ const db = require('../models');
 const request = require('request');
 const cheerio = require('cheerio');
 
-//GET the information
-router.get('/scrape', (req, res) => {
-  console.log('scraping commenced');
-  //grab body of html
-  request('https://www.nytimes.com/', (err, res, body) => {
-    if (!error && Response.statusCode === 200) {
-      const $ = cheerio.load(body);
-      let count = 0;
-      //Grab the articles from body
-      $('article').each(function (i, element ) {
-        let count = i;
-        let result = {};
-        //add the href and the text to each link, saving them
-        result.title = $('element')
-          .children('.story-heading')
-          .children('a')
-          .text().trim();
-        result.link = $('element')
-          .children('.story-heading')
-          .children('a')
-          .attr('href');
-        result.summary = $('element')
-          .children('.summary')
-          .text().trim() || $('element')
-            .children('ul')
-            .text().trim();
-        result.byline = $('element')
-          .children('.byline')
-          .text().trim() || 'no byline available'
-        
-        if (result.title && result.link && result.summary) {
-          db.Article.create(result)
-            .then(function (dbArticle) {
-              count++;
-            })
-            .catch(function (err) {
-              return res.json(err);
-            });
-        };
-      });
-      res.redirect('/');
-    }
-    else if (error || res.statusCode !=200) {
-      res.setEncoding('Error: Unable to obtain new articles.')
-    };
+// //GET the information
+router.get("/scrape", (req, res) => {
+  console.log("scrape ran")
+  // First, we grab the body of the html with request
+  request("https://www.nytimes.com/", (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+          // Then, we load that into cheerio and save it to $ for a shorthand selector
+          const $ = cheerio.load(body);
+          let count = 0;
+          // Now, we grab every article:
+          $('article').each(function (i, element) {
+              // Save an empty result object
+              let count = i;
+              let result = {};
+              // Add the text and href of every link, and summary and byline, saving them to object
+              result.title = $(element)
+                  .children('.story-heading')
+                  .children('a')
+                  .text().trim();
+              result.link = $(element)
+                  .children('.story-heading')
+                  .children('a')
+                  .attr("href");
+              result.summary = $(element)
+                  .children('.summary')
+                  .text().trim()
+                  || $(element)
+                      .children('ul')
+                      .text().trim();
+              result.byline = $(element)
+                  .children('.byline')
+                  .text().trim()
+                  || 'No byline available'
+              
+              if (result.title && result.link && result.summary){
+                  // Create a new Article using the `result` object built from scraping, but only if both values are present
+                  db.Article.create(result)
+                      .then(function (dbArticle) {
+                          // View the added result in the console
+                          count++;
+                      })
+                      .catch(function (err) {
+                          // If an error occurred, send it to the client
+                          return res.json(err);
+                      });
+              };
+          });
+          // If we were able to successfully scrape and save an Article, redirect to index
+          res.redirect('/')
+      }
+      else if (error || response.statusCode != 200){
+          res.send("Error: Unable to obtain new articles")
+      }
   });
 });
+
 
 router.get('/', (req, res) => {
     db.Article.find({})
